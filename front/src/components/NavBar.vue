99e37cd173af6817c781e4462f928e092f4ceacd
Jules <jules.davoust@yahoo.fr>
2023-09-28 08:07:46 +0200

    setup

---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="Navbar" class="d-flex justify-content-center align-items-center">
+        <nav class="navbar nb">
+            <a class="nav-link active" aria-current="page" href="/"><div class="accueil">Accueil</div></a>
+            <a class="nav-link active" aria-current="page" href="/cart"><div class="caddie">Mon panier</div></a>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'navBar',
+};
+</script>
+
+<style>
+#Navbar{
+    background-color: burlywood;
+    height: 10vh;
+}
+
+.nb {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+
+.accueil, .caddie{
+    font-size:150%;
+}
+
+.caddie {
+    margin-left:-15%;
+}
+.accueil {
+    margin-left:15%;
+}
+</style>
---
+++ front/src/views/Cart.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="cartView">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    test 1
+                </div>
+                <div class="col">
+                    test 2
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  methods: {
+
+  },
+  async created() {
+    console.log('created');
+    await fetch('http://localhost:3000/cart')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style>
+    .container{
+        border:2px solid black;
+    }
+    .col{
+        border:2px solid black;
+    }
+    .row{
+        border:2px solid black;
+    }
+</style>
